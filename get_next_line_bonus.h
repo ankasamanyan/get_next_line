920ebc31733bce44919c296b2b48ccf52d5559af
Anahit K <anikakasamanyan@gmail.com>
2022-04-02 16:50:21 +0300

    AAAAAAAAAAA

---
+++ get_next_line_bonus.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ankasamanyan <ankasamanyan@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 20:40:05 by ankasamanya       #+#    #+#             */
+/*   Updated: 2022/04/02 16:49:31 by ankasamanya      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+size_t	gnl_strlen(char *str);
+char	*gnl_calloc(int count);
+char	*gnl_strdup(char *str);
+char	*gnl_strchr(char *str, char c);
+char	*gnl_memmove(char *dest, char *src, size_t len);
+
+#endif
+// #  define BUFFER_SIZE 10
